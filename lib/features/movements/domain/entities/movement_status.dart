55ba4a65ceea6e8d5297f99f60b3898c98c402a4
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2026-06-01 00:19:50 -0500

    feat: add LocalizedMovementStatus extension for localized string representations

--- lib/features/movements/domain/entities/movement_status.dart
+++ lib/features/movements/domain/entities/movement_status.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:ligo_app/core/extensions/localization_extension.dart';
+
 /// Defines the status of a movement in the application.
 enum MovementStatus {
   /// Represents a movement that is pending and has not been completed yet.
@@ -11,4 +14,21 @@ enum MovementStatus {
 
   /// Represents a movement with an unknown status.
   unknown,
+}
+
+/// Extension on [MovementStatus] to provide localized string representations.
+extension LocalizedMovementStatus on MovementStatus {
+  /// Returns a localized string representation of the movement status.
+  String localized(BuildContext context) {
+    switch (this) {
+      case MovementStatus.pending:
+        return context.localized.pending;
+      case MovementStatus.completed:
+        return context.localized.completed;
+      case MovementStatus.failed:
+        return context.localized.failed;
+      case MovementStatus.unknown:
+        return context.localized.unknown;
+    }
+  }
 }
